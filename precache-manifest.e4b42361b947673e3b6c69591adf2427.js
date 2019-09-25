b544348c81671025fbecddad96f7ad1b1dd3a3e9
Andrés Felipe Naranjo Quintero l <andresnaranjo@afnarqui.com>
2019-09-24 20:01:08 -0500

    Updates

---
+++ precache-manifest.e4b42361b947673e3b6c69591adf2427.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56a085db70170ac4d804445c05cea1a",
+    "url": "/video/index.html"
+  },
+  {
+    "revision": "8e058ea8944c9c0e3a93",
+    "url": "/video/static/js/2.9ff96f52.chunk.js"
+  },
+  {
+    "revision": "443819f25d64bc05677f",
+    "url": "/video/static/js/main.89129b7b.chunk.js"
+  },
+  {
+    "revision": "9695152e3117de44e600",
+    "url": "/video/static/js/runtime~main.e0a31693.js"
+  }
+]);
